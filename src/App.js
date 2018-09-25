0c149627aafab9f767f1076648957da22a102860
hleank <hermanleank@gmail.com>
2018-09-25 14:43:29 -0500

    Grid Works

--- src/App.js
+++ src/App.js
@@ -6,56 +6,57 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 
-
 const styles = {
-  root: {
-    flexGrow: 1,
-  },
+	root: {
+		flexGrow: 1
+	},
+	paper: {
+		padding: '10%',
+		textAlign: 'center',
+		color: 'black'
+	}
 };
 
 class App extends Component {
-  state = {
-    value: 0,
-  };
+	state = {
+		value: 0
+	};
 
-  handleChange = (event, value) => {
-    this.setState({ value });
-  };
-  render() {
-    const { classes } = this.props;
-    return (
-      <div className="App">
-        <header className="App-header">
-        <AppBar position="static" color="default">
-        <Toolbar>
-          <Typography variant="Headline" color="inherit">
-           Header
-          </Typography>
-        </Toolbar>
-        <Paper className={classes.root}>
-        <Tabs
-          value={this.state.value}
-          onChange={this.handleChange}
-          indicatorColor="primary"
-          textColor="primary"
-          centered
-        >
-          <Tab label="Item One" />
-          <Tab label="Item Two" />
-          <Tab label="Item Three" />
-        </Tabs>
-      </Paper>
-      </AppBar>
-      </header>
-      </div>
-    );
-  }
+	handleChange = (event, value) => {
+		this.setState({ value });
+	};
+	render() {
+		const { classes } = this.props;
+		return (
+			<div className="App">
+       <div className="whole">
+				<header className="App-header">
+					<AppBar position="static" color="default">
+						<Paper className='Menubar' className={classes.root}>
+							<Tabs
+								value={this.state.value}
+								onChange={this.handleChange}
+								indicatorColor="primary"
+								textColor="primary"
+								centered
+							>
+								<Tab label="Item One" />
+								<Tab label="Item Two" />
+								<Tab label="Item Three" />
+							</Tabs>
+						</Paper>
+					</AppBar>
+				</header>
+          <div className='middle'>
+          </div>
+        </div>
+			</div>
+		);
+	}
 }
 
-
-
 export default withStyles(styles)(App);
-
